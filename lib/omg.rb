102a47e3ced01d9e5b6fc27264579889de013c95
Trevor Grayson <trevor@ipsumllc.com>
2013-04-28 19:30:18 -0400

    cleanup and getting things running

--- lib/omg.rb
+++ lib/omg.rb
@@ -1,6 +1,8 @@
 Dir["tasks/**/*.rake"].each { |ext| load ext } if defined?(Rake)
 
-config.autoload_paths += %W(#{config.root}/lib/auto_reload) 
+if Rails.env.development?
+  ActiveSupport::Dependencies.autoload_paths << %W(#{Rails.root}/lib/auto_reload) 
+end
 
 require 'omg/engine'
 require 'omg/acts_as_javascript'
--- lib/omg/builder.rb
+++ lib/omg/builder.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
-require 'opal'
 
 module Omg
   class Builder
