54b4646b180d181d85dd2b2247e83da73c309528
Trevor Grayson <trevor@ipsumllc.com>
2013-04-28 20:37:04 -0400

    ActiveModel => ActiveRecord. figure this out later

--- vendor/assets/javascripts/active_record.js
+++ vendor/assets/javascripts/active_record.js
@@ -1,13 +1,13 @@
 (function(__opal) {
   var self = __opal.top, __scope = __opal, nil = __opal.nil, $mm = __opal.mm, __breaker = __opal.breaker, __slice = __opal.slice, __klass = __opal.klass;
   return (function(__base, __super){
-    function ActiveModel() {};
-    ActiveModel = __klass(__base, __super, "ActiveModel", ActiveModel);
+    function ActiveRecord() {};
+    ActiveRecord = __klass(__base, __super, "ActiveRecord", ActiveRecord);
 
-    var def = ActiveModel.prototype, __scope = ActiveModel._scope;
+    var def = ActiveRecord.prototype, __scope = ActiveRecord._scope;
     var xmlDoc;
 
-    ActiveModel._defs('find', function(id) {
+    ActiveRecord._defs('find', function(id) {
       if (typeof window.ActiveXObject != 'undefined' ) {
         xmlDoc = new ActiveXObject("Microsoft.XMLHTTP");
         xmlDoc.onreadystatechange = this.build ;
@@ -21,19 +21,19 @@
 
     });
 
-    ActiveModel._defs('build', function(id) {
+    ActiveRecord._defs('build', function(id) {
       console.log('building')
       console.log(xmlDoc.responseText);
     });
 
-    ActiveModel._defs('url', function(id) {
+    ActiveRecord._defs('url', function(id) {
       var _a, _b, _c;if (id == null) {
         id = nil
       }
       return "/" + (((_a = ((_b = this).$pluralize || $mm('pluralize')).call(_b, ((_c = this).$name || $mm('name')).call(_c))).$downcase || $mm('downcase')).call(_a)) + "/" + (id)
     });
 
-    ActiveModel._defs('$pluralize', function(word) {
+    ActiveRecord._defs('$pluralize', function(word) {
       
       return "" + (word) + "s"
     });
